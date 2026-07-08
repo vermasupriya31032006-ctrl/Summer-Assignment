49f146481a4c297b42b558689427207f113feb7d
Supriya Verma <vermasupriya31032006@gmail.com>
2026-07-08 19:20:20 +0530

    Update TwoSum.cpp

--- Week1/Day1/TwoSum.cpp
+++ Week1/Day1/TwoSum.cpp
@@ -1,8 +1,19 @@
-for(int i=0;i<nums.size();i++)
-{
-    for(int j=i+1;j<nums.size();j++)
-    {
-        if(nums[i]+nums[j]==target)
-            return {i,j};
+class Solution {
+public:
+
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        for(int i=0;i<nums.size();i++)
+        {
+            for(int j=i+1;j<nums.size();j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                    return {i,j};
+                }
+            }
+        }
+
+        return {};
     }
-}
+};
