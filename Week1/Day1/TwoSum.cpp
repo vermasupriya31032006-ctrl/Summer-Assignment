259e099a1edd89d8ec6a710b9ef60970f79b362c
Supriya Verma <vermasupriya31032006@gmail.com>
2026-07-08 19:17:58 +0530

    Update TwoSum.cpp

--- Week1/Day1/TwoSum.cpp
+++ Week1/Day1/TwoSum.cpp
@@ -1,24 +1,8 @@
-#include <vector>
-#include <unordered_map>
-using namespace std;
-
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-
-        unordered_map<int, int> mp;
-
-        for (int i = 0; i < nums.size(); i++) {
-
-            int complement = target - nums[i];
-
-            if (mp.find(complement) != mp.end()) {
-                return {mp[complement], i};
-            }
-
-            mp[nums[i]] = i;
-        }
-
-        return {};
+for(int i=0;i<nums.size();i++)
+{
+    for(int j=i+1;j<nums.size();j++)
+    {
+        if(nums[i]+nums[j]==target)
+            return {i,j};
     }
-};
+}
