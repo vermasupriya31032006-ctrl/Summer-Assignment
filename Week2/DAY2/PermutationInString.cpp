5c84e95e1e2194eaa7993295dae42b3200c058dd
Supriya Verma <vermasupriya31032006@gmail.com>
2026-07-09 22:55:35 +0530

    Update PermutationInString.cpp

--- Week2/DAY2/PermutationInString.cpp
+++ Week2/DAY2/PermutationInString.cpp
@@ -1,1 +1,20 @@
 
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+
+        sort(s1.begin(), s1.end());
+
+        for(int i = 0; i <= s2.length() - s1.length(); i++) {
+
+            string temp = s2.substr(i, s1.length());
+
+            sort(temp.begin(), temp.end());
+
+            if(temp == s1)
+                return true;
+        }
+
+        return false;
+    }
+};
