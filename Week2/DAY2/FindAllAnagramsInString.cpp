a0fabdfdd88ab09dc72926cce07d98ab69f4fb84
Supriya Verma <vermasupriya31032006@gmail.com>
2026-07-09 22:53:32 +0530

    Update FindAllAnagramsInString.cpp

--- Week2/DAY2/FindAllAnagramsInString.cpp
+++ Week2/DAY2/FindAllAnagramsInString.cpp
@@ -1,1 +1,33 @@
 
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+
+        vector<int> ans;
+
+        if (s.length() < p.length())
+            return ans;
+
+        vector<int> countP(26, 0);
+        vector<int> countS(26, 0);
+
+        for (int i = 0; i < p.length(); i++) {
+            countP[p[i] - 'a']++;
+            countS[s[i] - 'a']++;
+        }
+
+        if (countP == countS)
+            ans.push_back(0);
+
+        for (int i = p.length(); i < s.length(); i++) {
+
+            countS[s[i] - 'a']++;
+            countS[s[i - p.length()] - 'a']--;
+
+            if (countP == countS)
+                ans.push_back(i - p.length() + 1);
+        }
+
+        return ans;
+    }
+};
