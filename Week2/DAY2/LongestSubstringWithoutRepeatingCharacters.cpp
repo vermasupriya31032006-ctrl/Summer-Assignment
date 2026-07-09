2b6f8b370f93d93d598ffd11e52ade27a26f698f
Supriya Verma <vermasupriya31032006@gmail.com>
2026-07-09 22:58:02 +0530

    Update LongestSubstringWithoutRepeatingCharacters.cpp

--- Week2/DAY2/LongestSubstringWithoutRepeatingCharacters.cpp
+++ Week2/DAY2/LongestSubstringWithoutRepeatingCharacters.cpp
@@ -1,1 +1,26 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
 
+        int maxLength = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+
+            vector<bool> visited(256, false);
+            int count = 0;
+
+            for(int j = i; j < s.length(); j++) {
+
+                if(visited[s[j]])
+                    break;
+
+                visited[s[j]] = true;
+                count++;
+            }
+
+            maxLength = max(maxLength, count);
+        }
+
+        return maxLength;
+    }
+};
