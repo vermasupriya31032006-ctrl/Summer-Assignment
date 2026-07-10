696727d5c8d109d9a770bc5de825599907c80417
Supriya Verma <vermasupriya31032006@gmail.com>
2026-07-10 22:52:04 +0530

    Update FindFirstOccurrence.cpp

--- Week2/DAY2/FindFirstOccurrence.cpp
+++ Week2/DAY2/FindFirstOccurrence.cpp
@@ -1,1 +1,21 @@
 
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+
+        for(int i = 0; i <= haystack.size() - needle.size(); i++)
+        {
+            int j = 0;
+
+            while(j < needle.size() && haystack[i + j] == needle[j])
+            {
+                j++;
+            }
+
+            if(j == needle.size())
+                return i;
+        }
+
+        return -1;
+    }
+};
