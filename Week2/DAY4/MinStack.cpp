6c18bda2ffa99a73e73d32a8755d8f492c38d9b9
Supriya Verma <vermasupriya31032006@gmail.com>
2026-07-11 23:32:09 +0530

    Update MinStack.cpp

--- Week2/DAY4/MinStack.cpp
+++ Week2/DAY4/MinStack.cpp
@@ -1,1 +1,36 @@
+class MinStack {
+public:
 
+    stack<int> st;
+    stack<int> minSt;
+
+    MinStack() {
+
+    }
+
+    void push(int val) {
+
+        st.push(val);
+
+        if(minSt.empty())
+            minSt.push(val);
+        else
+            minSt.push(min(val,minSt.top()));
+    }
+
+    void pop() {
+
+        st.pop();
+        minSt.pop();
+    }
+
+    int top() {
+
+        return st.top();
+    }
+
+    int getMin() {
+
+        return minSt.top();
+    }
+};
