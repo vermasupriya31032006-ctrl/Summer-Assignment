6bd626d3015dd05f18009f051e6a8150d2fc19b4
Supriya Verma <vermasupriya31032006@gmail.com>
2026-07-13 22:50:46 +0530

    Update PalindromeLinkedList.cpp

--- Week_3/DAY 2/PalindromeLinkedList.cpp
+++ Week_3/DAY 2/PalindromeLinkedList.cpp
@@ -1,1 +1,27 @@
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
 
+        vector<int> arr;
+
+        while(head != NULL)
+        {
+            arr.push_back(head->val);
+            head = head->next;
+        }
+
+        int left = 0;
+        int right = arr.size() - 1;
+
+        while(left < right)
+        {
+            if(arr[left] != arr[right])
+                return false;
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+};
