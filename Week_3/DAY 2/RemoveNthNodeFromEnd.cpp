af27842e3dd8d85c2dc36dfbcf9402dc8eb6f200
Supriya Verma <vermasupriya31032006@gmail.com>
2026-07-13 22:33:34 +0530

    Update RemoveNthNodeFromEnd.cpp

--- Week_3/DAY 2/RemoveNthNodeFromEnd.cpp
+++ Week_3/DAY 2/RemoveNthNodeFromEnd.cpp
@@ -1,1 +1,29 @@
 
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+
+        int count = 0;
+        ListNode* temp = head;
+
+        while(temp)
+        {
+            count++;
+            temp = temp->next;
+        }
+
+        if(count == n)
+            return head->next;
+
+        temp = head;
+
+        for(int i = 0; i < count - n - 1; i++)
+        {
+            temp = temp->next;
+        }
+
+        temp->next = temp->next->next;
+
+        return head;
+    }
+};
