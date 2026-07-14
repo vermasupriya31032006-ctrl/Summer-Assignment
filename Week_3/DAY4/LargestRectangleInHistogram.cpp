134a0b43f59a122e54ec92c77f2d49a51c9217a5
Supriya Verma <vermasupriya31032006@gmail.com>
2026-07-14 23:26:01 +0530

    Update LargestRectangleInHistogram.cpp

--- Week_3/DAY4/LargestRectangleInHistogram.cpp
+++ Week_3/DAY4/LargestRectangleInHistogram.cpp
@@ -1,1 +1,34 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
 
+        stack<int> st;
+
+        for (string s : tokens)
+        {
+            if (s == "+" || s == "-" || s == "*" || s == "/")
+            {
+                int num2 = st.top();
+                st.pop();
+
+                int num1 = st.top();
+                st.pop();
+
+                if (s == "+")
+                    st.push(num1 + num2);
+                else if (s == "-")
+                    st.push(num1 - num2);
+                else if (s == "*")
+                    st.push(num1 * num2);
+                else
+                    st.push(num1 / num2);
+            }
+            else
+            {
+                st.push(stoi(s));
+            }
+        }
+
+        return st.top();
+    }
+};
